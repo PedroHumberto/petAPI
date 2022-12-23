ba773764b1ffee26a4e42c27d81206f519feabe8
Pedro Humberto <78572029+PedroHumberto@users.noreply.github.com>
2022-12-22 21:32:53 -0300

    iddentation corrections

--- Controllers/PetController.cs
+++ Controllers/PetController.cs
@@ -13,35 +13,33 @@ public class PetController : ControllerBase
     private AppDbContext _context;
     private IMapper _mapper;
 
-    public PetController(AppDbContext context, IMapper mapper)
-    {
+    public PetController(AppDbContext context, IMapper mapper){
         _context = context;
         _mapper = mapper;
     }
 
     [HttpPost]
-    public IActionResult AddPet([FromBody] CreatePetDto petDto)
-    {
-        Pet pet = _mapper.Map<Pet>(petDto);
-
-        _context.Pets.Add(pet);
-        _context.SaveChanges();
-        return CreatedAtAction(nameof(GetPetById), new { Id = pet.Id }, pet); // "https://localhost:5000/Pets/1"
-    }
-
-    [HttpGet("{id}")]
-    public IActionResult GetPetById(int id)
-    {
-        Pet pet = _context.Pets.FirstOrDefault(filme => filme.Id == id);
-
-        if (pet != null)
+        public IActionResult AddPet([FromBody] CreatePetDto petDto)
         {
-            ReadPetDto petDto = _mapper.Map<ReadPetDto>(pet);
-            return Ok(petDto);
+            Pet pet = _mapper.Map<Pet>(petDto);
+
+            _context.Pets.Add(pet);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetPetById), new { Id = pet.Id }, pet); // "https://localhost:5000/Pets/1"
         }
-        else
+
+        [HttpGet("{id}")]
+        public IActionResult GetPetById(int id)
         {
-            return NotFound();
+            Pet pet = _context.Pets.FirstOrDefault(filme => filme.Id == id);
+            if (pet != null)
+            {
+                ReadPetDto petDto = _mapper.Map<ReadPetDto>(pet);
+                return Ok(petDto);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
-    }
 }
